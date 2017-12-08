54ae11515cb247403c8eb984ce460e1c832bb18c
siamcr7 <siam_cr7@yahoo.com>
2017-12-09 00:35:52 +0600

    Veiw Update

--- authenticated/allInOneViewPage.php
+++ authenticated/allInOneViewPage.php
@@ -19,22 +19,42 @@
 		</td>
 			
 		<td colspan="2" width = "500">
-			
-			<fieldset>
-				<legend>View Page</legend>			
-				<table style="width:100%">
-					<tr>
-						<td>ID :</td>
-						<td>1</td>
-					</tr>
-					
-					<tr>
-						<td>Name :</td>
-						<td>XYZ</td>
-					</tr>
-				</table>
-			</fieldset>
-			
+			<?php
+				/*
+				Make the dynamic table more dynamic so that:
+				we can print any colomb name as we wish by giving in the first value.
+				Also find a way to print the specific col value only. one way is to build
+				another arr and send it. For now using only the static values to show.
+				*/
+				
+				include("../DynamicTable/index.php");
+				buildDynamicTable($_SESSION["userList"]);
+				viewDynamicTableInHTML(true);
+				*/
+				
+				/*
+				$userArr["Name"] = "Jamil Siam";
+				$userArr["Email"] = "siam_cr7@yahoo.com";
+				$userArr["User Name"] = "siam_cr7";
+				$userList[0] = $userArr;
+				
+				
+				$userArr["Name"] = "John Doe";
+				$userArr["Email"] = "siam@yahoo.com";
+				$userArr["User Name"] = "cr7";
+				$userList[1] = $userArr;
+				
+				$userArr["Name"] = "Amit";
+				$userArr["Email"] = "Amit@yahoo.com";
+				$userArr["User Name"] = "amt";
+				$userList[2] = $userArr;
+				
+				include("../DynamicTable/index.php");
+				buildDynamicTable($userList);
+				viewDynamicTableInHTML(true);
+				*/
+				
+			?>
 		</td>
 	</tr>
 	
