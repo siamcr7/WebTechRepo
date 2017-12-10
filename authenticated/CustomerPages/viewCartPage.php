febafc648aa9c1852f82c298dde35c892df35a94
siamcr7 <siam_cr7@yahoo.com>
2017-12-11 00:51:13 +0600

    okay

---
+++ authenticated/CustomerPages/viewCartPage.php
@@ -0,0 +1,91 @@
+<?php
+	session_start();
+?>
+
+<table style="width:100%" , border = "1">
+			
+	<tr>
+		<td colspan = "3">
+			<?php
+				include("header.php");
+			?>	
+		</td>
+	</tr>
+		  
+	<tr>
+		<td colspan="1">
+			<?php
+				include("accountCol.php");
+			?>
+		</td>
+			
+		<td colspan="2" width = "500">
+			<h1>View Cart List</h1>
+			<?php
+				$imgLoc = "/WebTechRepo/authenticated/Resources/FoodPic/";
+				
+				$tot = 0;
+				
+				$iCnt = 0; //$_COOKIE["curPage"];
+				$userArr["Picture"] = $imgLoc."{$iCnt}.png";
+				$userArr["Food Name"] = "Choco Burger";
+				$userArr["Food Type"] = "Burger";
+				$userArr["Ingredients"] = "Bread, Beef, Jellapino, Cheese";
+				$userArr["Price (TK)"] = "280";
+				$userArr["Rating"] = "4.8";
+				$userArr["Quantity"] = "2";
+				$userArr["Select"] = "checked";
+				$userList[$iCnt++] = $userArr;
+				$tot += ($userList[$iCnt-1]["Price (TK)"] * $userList[$iCnt-1]["Quantity"]);
+				
+				
+				$userArr["Picture"] = $imgLoc."{$iCnt}.png";
+				$userArr["Food Name"] = "Big Burger";
+				$userArr["Food Type"] = "Burger";
+				$userArr["Ingredients"] = "Bread, Beef, Jellapino, Cheese";
+				$userArr["Price (TK)"] = "280";
+				$userArr["Rating"] = "4.8";
+				$userArr["Quantity"] = "6";
+				$userArr["Select"] = "checked";
+				$userList[$iCnt++] = $userArr;
+				$tot += ($userList[$iCnt-1]["Price (TK)"] * $userList[$iCnt-1]["Quantity"]);
+				
+				$userArr["Picture"] = $imgLoc."{$iCnt}.png";
+				$userArr["Food Name"] = "Pizza Burger";
+				$userArr["Food Type"] = "Burger";
+				$userArr["Ingredients"] = "Bread, Beef, Jellapino, Cheese";
+				$userArr["Price (TK)"] = "280";
+				$userArr["Rating"] = "4.8";
+				$userArr["Quantity"] = "1";
+				$userArr["Select"] = "checked";
+				$userList[$iCnt++] = $userArr;
+				$tot += ($userList[$iCnt-1]["Price (TK)"] * $userList[$iCnt-1]["Quantity"]);
+				
+				include("../DynamicTable/index.php");
+				buildDynamicTable($userList);
+				viewDynamicTableinHTMLFromCustomer();
+			?>
+			
+			
+			<h2 align="center">
+				TOTAL PRICE = <?=$tot;?> BDT
+			</h2>
+			
+			<p align="center" > <a href = "orderConfirmPage.php" >Go To Order Confirm Page</a> </p>
+			
+			
+			
+		</td>
+	</tr>
+	
+	<tr>
+		<td colspan = "3">
+			<?php
+				include("footer.php");
+			?>	
+		</td>
+	</tr>
+		 
+</table>
+
+
