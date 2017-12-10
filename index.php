1e61a5772d3ab6c78ad995ae1c5f76399a41e6f0
siamcr7 <siam_cr7@yahoo.com>
2017-12-11 01:36:07 +0600

    a

---
+++ index.php
@@ -0,0 +1,25 @@
+<table style="width:100%" , border = "1">
+			
+	<tr>
+		<td colspan = "3">
+			<?php
+				include("header.php");
+			?>
+		</td>
+	</tr>
+		  
+	<tr>
+		<td  colspan="3"  height="100">
+			<h1 align="left">Welcome to company name</h1>
+		</td>
+	</tr>
+	
+	<tr>
+		<td colspan = "3">
+			<?php
+				include("footer.php");
+			?>
+		</td>
+	</tr>
+	
+</table>
