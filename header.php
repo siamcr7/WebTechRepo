cbeeddf0945656ef1282e867aa2f8ff0cf04d5aa
siamcr7 <siam_cr7@yahoo.com>
2017-12-12 13:16:01 +0600

    Preparing for new strong architecture

--- header.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<h1 align="left">Company Name</h1>
-	<p align="right"><a href = "index.php" >Home</a>
-	 | 
-	<a href = "loginPage.php" align="right">Login</a>
-	 |
-	<a href = "RegisterPage.php" align="right">Register</a></p>
-</html>
